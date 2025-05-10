965bcc489153c5ef4135aafebd9b20a5ff34d494
JacobersCom <jacobymartin1@icloud.com>
2025-05-10 16:35:14 -0400

    Damage Script | Mage Idle State
    I created the Damage Script, Not finished but will be, Started on mage enemy animation.

--- Prototype/Assets/Scripts/enemy/Damage.cs
+++ Prototype/Assets/Scripts/enemy/Damage.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+    enum damageType
+    {
+        moving, homing, DOT, stationary
+    }
+
+    [SerializeField] damageType type;
+
+    [SerializeField] Rigidbody body;
+
+    [SerializeField] int damageAmount;
+    [SerializeField] int damageRate;
+    [SerializeField] int speed;
+    [SerializeField] int destoryTime;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
