23fb99add3645db6d89b787c89cb80e565270643
TikiToad <zstoneburnerTMOS@gmail.com>
2025-05-13 21:16:20 -0400

    Updated SpellList.cs

--- Prototype/Assets/Scripts/SpellList.cs
+++ Prototype/Assets/Scripts/SpellList.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 
+//Required in order to use lists in C#
+using System.Collections.Generic;
+
+
 public class SpellList : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Created a spell class
+    class Spell
+    {
+    //Field to set the name of the spells
+        [SerializeField] string name;
+    }
+    //The spell objects
+    Spell fireball;
+    Spell callLightning;
+    Spell summonSkeleton;
+
+    //A list containing the spells
+    List<Spell> spellList = new List<Spell>();
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //Adding spells to the list once the game initializes 
+        spellList.Add(fireball);
+        spellList.Add(callLightning);
+        spellList.Add(summonSkeleton);
     }
 }
