540526dafd9c595e8a44247b7a691a5e273367df
Draycos2003 <c.strappello@gmail.com>
2025-05-21 14:25:00 -0400

    Update enemyAI script
    Add code for bool CanSeePlayer and associated vars

--- Prototype/Assets/Scripts/enemy/enemyAI.cs
+++ Prototype/Assets/Scripts/enemy/enemyAI.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
 
 
 public class enemyAI : MonoBehaviour, IDamage
 {
+    [SerializeField] private Transform headPos;
+    [SerializeField] private int FOV;
+    [SerializeField] private NavMeshAgent agent;
+
+    private Vector3 playerDir;
+    private float angleToPlayer;
+    private float shootTimer;
+    private float stoppingDistOrig;
+
     public enum EnemyTypes
     {
-        Range, 
+        Range,
         Melee,
     }
 
@@ -29,33 +39,37 @@ public class enemyAI : MonoBehaviour, IDamage
     public Transform shootPos;
     public GameObject bullet;
     public float shootRate;
-    bool inRange;
+    private bool inRange;
 
     [Header("Melee Fields")]
     public float attackSpeed;
     public GameObject weapon;
     public Collider hitPos;
 
-    Color colorOrig;
+    private Color colorOrig;
 
     private EnemyReferences references;
 
-    void Start()
+    private void Start()
     {
         references = GetComponent<EnemyReferences>();
         colorOrig = model.material.color; // Starter color
         gamemanager.instance.updateGameGoal(1); // total enemy count
+        stoppingDistOrig = agent.stoppingDistance;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (target != null)
         {
 
             if (inRange == true)
             {
-                references.animate.SetBool("casting", inRange);
+                if (CanSeePlayer())
+                {
+                    references.animate.SetBool("casting", inRange);
+                }
 
             }
             else
@@ -82,10 +96,41 @@ public class enemyAI : MonoBehaviour, IDamage
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag ==("Player"))
+        if (other.tag == ("Player"))
         {
             inRange = false;
         }
+    }
+
+    bool CanSeePlayer()
+    {
+        playerDir = (gamemanager.instance.Player.transform.position - headPos.position);
+        angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
+        Debug.DrawRay(headPos.position, new Vector3(playerDir.x, 0, playerDir.z));
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
+            {
+                agent.SetDestination(gamemanager.instance.Player.transform.position);
+
+                if (shootTimer >= shootRate)
+                {
+                    shoot();
+                }
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    faceTarget();
+                }
+
+                agent.stoppingDistance = stoppingDistOrig;
+                return true;
+            }       
+        }
+        agent.stoppingDistance = 0;
+        return false;
     }
 
     public void TakeDamage(int Amount)
@@ -111,33 +156,33 @@ public class enemyAI : MonoBehaviour, IDamage
         }
     }
 
-    IEnumerator flashRed()
+    private IEnumerator flashRed()
     {
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.05f);
         model.material.color = colorOrig;
     }
 
-    void faceTarget()
+    private void faceTarget()
     {
-//      --  Creates a smoother rotation by using Slerp.
-//      -- I use Slerp instead of lerp because i don't know what type of rotation the character could make it could be big but if not, it could be juddery using lerp so be safe with Slerp.
+        //      --  Creates a smoother rotation by using Slerp.
+        //      -- I use Slerp instead of lerp because i don't know what type of rotation the character could make it could be big but if not, it could be juddery using lerp so be safe with Slerp.
 
-        Vector3 lookPos = target.position - transform.position;
-        lookPos.y = 0;
-        Quaternion rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, faceTargetSpeed * Time.deltaTime);
+        //Vector3 lookPos = target.position - transform.position;
+        //lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * faceTargetSpeed);
 
     }
 
-    void shoot()
+    private void shoot()
     {
         Instantiate(bullet, shootPos.position, transform.rotation);
     }
 
     private void UpdatePath()
     {
-//      -- Updates the Path every 0.2 seconds instead of every frame like navMesh.SetDestination(target.postion)
+        //      -- Updates the Path every 0.2 seconds instead of every frame like navMesh.SetDestination(target.postion)
         if (Time.time >= updatePathDeadline)
         {
             Debug.Log("Updating Path");
