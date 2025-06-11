a4a24245a113f7446a2f56e2591e183830ccd443
Zoey <68152997+AspenXIV@users.noreply.github.com>
2025-06-11 16:13:54 -0400

    Added door to showcase

--- Prototype/Assets/Scripts/Utility/Command bar/Commands/ShowcaseCommand.cs
+++ Prototype/Assets/Scripts/Utility/Command bar/Commands/ShowcaseCommand.cs
@@ -16,6 +16,6 @@ public class ShowcaseCommand : IConsoleCommand
     public void Execute(string[] args)
     {
         _log("Loading Showcase scene...");
-        SceneManager.LoadScene("Tutorial");
+        SceneManager.LoadScene("Showcase");
     }
 }
